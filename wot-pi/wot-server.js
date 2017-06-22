9420d11bb83bc4f272d36d57e7d716c56a80a331
Lars Van Holsbeeke <lavholsb@vub.ac.be>
2017-06-22 18:41:00 +0200

    temp comment out ledsplugin

--- wot-pi/wot-server.js
+++ wot-pi/wot-server.js
@@ -2,14 +2,14 @@ var httpServer = require('./servers/http'),
     resources = require('./resources/model');
 
 // Internal Plugins
-var ledsPlugin = require('./plugins/internal/ledsPlugin'), //#A
+var //ledsPlugin = require('./plugins/internal/ledsPlugin'), //#A
     pirPlugin = require('./plugins/internal/pirPlugin'), //#A
     dhtPlugin = require('./plugins/internal/DHT22SensorPlugin'); //#A
 
 // Internal Plugins for sensors/actuators connected to the PI GPIOs
 // If you test this with real sensors do not forget to set simulate to 'false'
 pirPlugin.start({'simulate': true, 'frequency': 2000});
-ledsPlugin.start({'simulate': true, 'frequency': 10000});
+//ledsPlugin.start({'simulate': true, 'frequency': 10000});
 dhtPlugin.start({'simulate': true, 'frequency': 10000});
 
 
