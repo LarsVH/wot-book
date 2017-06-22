e7c64779ba3b898ec8269e7e6f8e157938a75646
Lars Van Holsbeeke <lavholsb@vub.ac.be>
2017-06-22 18:44:34 +0200

    no more simulation :)

--- wot-pi/wot-server.js
+++ wot-pi/wot-server.js
@@ -8,9 +8,9 @@ var //ledsPlugin = require('./plugins/internal/ledsPlugin'), //#A
 
 // Internal Plugins for sensors/actuators connected to the PI GPIOs
 // If you test this with real sensors do not forget to set simulate to 'false'
-pirPlugin.start({'simulate': true, 'frequency': 2000});
+pirPlugin.start({'simulate': false, 'frequency': 2000});
 //ledsPlugin.start({'simulate': true, 'frequency': 10000});
-dhtPlugin.start({'simulate': true, 'frequency': 10000});
+dhtPlugin.start({'simulate': false, 'frequency': 10000});
 
 
 var server = httpServer.listen(resources.pi.port, function () {     // Listen on port with function
